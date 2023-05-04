f53464d8c3b77c7409e7d81059fa59e3af8a23c8
RiMiDo420 <richard.dobisek@mensagymnazium.cz>
2023-05-04 16:14:52 +0200

    Add project files.

---
+++ MLTest/Program.cs
@@ -0,0 +1,14 @@
+﻿
+//Load sample data
+using MLTest;
+
+var sampleData = new SentimentModel.ModelInput()
+{
+    Col0 = @"Wow... Loved this place.",
+};
+
+//Load model and predict output
+var result = SentimentModel.Predict(sampleData);
+
+var sentiment = result.PredictedLabel == 1 ? "Positive" : "Negative";
+Console.WriteLine($"Text: {sampleData.Col0}\nSentiment: {sentiment}");
